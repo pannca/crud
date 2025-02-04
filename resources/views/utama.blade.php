778a1bc23b851b8d7025229f4d3bf530f88e61aa
pannca <pancaprasetia@smkwikrama.sch.id>
2025-02-04 13:57:23 +0700

    fix : mengubah tampilan utama

--- resources/views/utama.blade.php
+++ resources/views/utama.blade.php
@@ -4,13 +4,18 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <title>Kelola Data Siswa</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
-<body class="d-flex align-items-center justify-content-center" style="height: 100vh; margin: 0;">
+<body class="d-flex flex-column align-items-center justify-content-center vh-100 bg-light">
 
-    <a href="{{ route('siswa.index') }}">
-        <button class="btn btn-primary">Letsgo</button>
-    </a>
+    <div class="card shadow-lg p-4" style="max-width: 500px; width: 100%;">
+        <div class="card-body text-center">
+            <h1 class="card-title mb-3 text-primary">Kelola Data Siswa</h1>
+            <p class="card-text text-muted">Kelola data siswa dengan mudah dan efisien. Akses informasi secara cepat dan terstruktur untuk pengalaman yang lebih baik.</p>
+            <a href="{{ route('siswa.index') }}" class="btn btn-primary w-100 mt-3">Lihat Data Siswa</a>
+        </div>
+    </div>
+
 </body>
 </html>
