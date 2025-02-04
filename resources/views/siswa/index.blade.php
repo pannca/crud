f59ae11c904e462cd3f086b9ad2529ba041d63ce
pannca <pancaprasetia@smkwikrama.sch.id>
2025-02-04 14:47:05 +0700

    fix : mengubah tampilan index

--- resources/views/siswa/index.blade.php
+++ resources/views/siswa/index.blade.php
@@ -6,69 +6,67 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Kelola Siswa</title>
-    <script src="https://code.jquery.com/jquery-3.7.1.min.js"
-        integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js" crossorigin="anonymous"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 </head>
 
-{{-- sweet alert --}}
-<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-<script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
+<body class="bg-light">
+    <div class="container mt-5">
+        <h2 class="text-center text-primary">Manajemen Data Siswa</h2>
+        <hr>
+        @if (Session::get('success'))
+            <div class="alert alert-success">{{ Session::get('success') }}</div>
+        @endif
 
-<body>
+        <div class="d-flex justify-content-between mb-3">
+            <h4>Daftar Siswa</h4>
+            <a href="{{ route('siswa.create') }}" class="btn btn-success">Tambah Data</a>
+        </div>
 
-    @if (Session::get('success'))
-        <div class="alert alert-success">{{ Session::get('success') }}</div>
-    @endif
-
-    <div class="p-5">
-        <a href=" {{ route('siswa.create') }}"><button type="button" class="btn btn-success">Tambah Data</button></a>
-        <table class="table">
-            <thead>
-                <tr>
-                    <th scope="col">Nama</th>
-                    <th scope="col">Rayon</th>
-                    <th scope="col">Rombel</th>
-                    <th scope="col">Nis</th>
-                    <th scope="col">Aksi</th>
-                </tr>
-            </thead>
-            <tbody>
-                @forelse ($siswa as $item)
+        <div class="table-responsive">
+            <table class="table table-bordered">
+                <thead class="table-dark text-white">
                     <tr>
-                        <td>{{ $item->nama }}</td>
-                        <td>{{ $item->rayon->nama_rayon }}</td>
-                        <td>{{ $item->rombel }}</td>
-                        <td>{{ $item->nis }}</td>
-                        <td class="d-flex">
-                            <a href="{{ route('siswa.edit', $item['id']) }}" class="btn btn-primary me-2">Edit</a>
-                            <form action="{{ route('siswa.hapus', $item['id']) }}" method="POST">
-                                @csrf
-                                @method('DELETE')
-                                <button type="button" class="btn btn-danger hapus"
-                                    data-id="{{ $item['id'] }}">Delete</button>
-                            </form>
-                        </td>
+                        <th>Nama</th>
+                        <th>Rayon</th>
+                        <th>Rombel</th>
+                        <th>Nis</th>
+                        <th>Aksi</th>
                     </tr>
-            </tbody>
-        @empty
-            <tbody>
-                <tr>
-                    <td>tidak ada data tersedia</td>
-                </tr>
-                @endforelse
-            </tbody>
-        </table>
+                </thead>
+                <tbody>
+                    @forelse ($siswa as $item)
+                        <tr>
+                            <td>{{ $item->nama }}</td>
+                            <td>{{ $item->rayon->nama_rayon }}</td>
+                            <td>{{ $item->rombel }}</td>
+                            <td>{{ $item->nis }}</td>
+                            <td class="d-flex">
+                                <a href="{{ route('siswa.edit', $item['id']) }}" class="btn btn-primary btn-sm me-2">Edit</a>
+                                <form action="{{ route('siswa.hapus', $item['id']) }}" method="POST">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button type="button" class="btn btn-danger btn-sm hapus" data-id="{{ $item['id'] }}">Hapus</button>
+                                </form>
+                            </td>
+                        </tr>
+                    @empty
+                        <tr>
+                            <td colspan="5" class="text-center text-muted">Tidak ada data tersedia</td>
+                        </tr>
+                    @endforelse
+                </tbody>
+            </table>
+        </div>
     </div>
 </body>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
-    integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
-</script>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
 <script>
     $(document).on('click', '.hapus', function(e) {
         e.preventDefault();
-        let form = $(this).closest('form'); // Cari form terdekat
+        let form = $(this).closest('form');
         Swal.fire({
             title: 'Apakah Anda yakin?',
             text: "Data yang dihapus tidak bisa dikembalikan!",
@@ -80,7 +78,7 @@
             cancelButtonText: 'Batal'
         }).then((result) => {
             if (result.isConfirmed) {
-                form.submit(); // Submit form setelah konfirmasi
+                form.submit();
             }
         });
     });
